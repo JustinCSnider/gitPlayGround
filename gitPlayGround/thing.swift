8b734d93a0da3e1c25e60cb6d1e8a146d5a66637
Justin Snider <jcsnider100@gmail.com>
2018-09-19 10:05:23 -0600

    Fixed navigation controller

---
+++ gitPlayGround/thing.swift
@@ -0,0 +1,17 @@
+//
+//  myStuff.swift
+//  gitPlayGround
+//
+//  Created by Justin Snider on 9/19/18.
+//  Copyright © 2018 Justin Snider. All rights reserved.
+//
+
+import Foundation
+
+class thing {
+    var name: String
+    
+    init?(name: String) {
+        self.name = name
+    }
+}
